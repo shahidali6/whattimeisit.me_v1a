1a1dcaa001aa7af6f0c3a3b921d5b0412b37a6bd
Shahid Saddique <shahidali6@gmail.com>
2024-08-25 01:19:00 +0500

    removed error from the time.js file

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,56 +1,3 @@
-// script.js
-const timeZonesAll = Intl.supportedValuesOf("timeZone");
-
-// Filter time zones of Africa only
-const timeZonesAfrica = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Africa/")
-);
-
-// Filter time zones of America only
-const timeZonesAmerica = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("America/")
-);
-
-// Filter time zones of Antarctica only
-const timeZonesAntarctica = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Antarctica/")
-);
-
-// Filter time zones of Arctic only
-const timeZonesArctic = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Arctic/")
-);
-
-// Filter time zones of Asia only
-const timeZonesAsia = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Asia/")
-);
-
-// Filter time zones of Atlantic only
-const timeZonesAtlantic = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Atlantic/")
-);
-
-// Filter time zones of Australia only
-const timeZonesAustralia = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Australia/")
-);
-
-// Filter time zones of Europe only
-const timeZonesEurope = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Europe/")
-);
-
-// Filter time zones of Indian only
-const timeZonesIndian = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Indian/")
-);
-
-// Filter time zones of Pacific only
-const timeZonesPacific = timeZonesAll.filter((timeZone) =>
-  timeZone.startsWith("Pacific/")
-);
-
 function updateDateTime() {
   const now = new Date();
   const dateTimeString = now.toLocaleString();
@@ -327,25 +274,25 @@ function updatePacificZoneTime() {
   });
 }
 
-function updateAllTimeZones() {
-  timeZonesAll.forEach((timeZone) => {
-    const now = new Date();
-    const options = {
-      timeZone,
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    };
-    const timeString = new Intl.DateTimeFormat("en-US", options).format(now);
-    const timeZoneId = `time${timeZone.replace(/\//g, "_")}`; // Replace '/' with '_' for valid HTML id
-    const timeCell = document.getElementById(timeZoneId);
-    if (timeCell) {
-      timeCell.textContent = timeString;
-    } else {
-      console.error(`Element with id ${timeZoneId} not found`);
-    }
-  });
-}
+// function updateAllTimeZones() {
+//   timeZonesAll.forEach((timeZone) => {
+//     const now = new Date();
+//     const options = {
+//       timeZone,
+//       hour: "2-digit",
+//       minute: "2-digit",
+//       second: "2-digit",
+//     };
+//     const timeString = new Intl.DateTimeFormat("en-US", options).format(now);
+//     const timeZoneId = `time${timeZone.replace(/\//g, "_")}`; // Replace '/' with '_' for valid HTML id
+//     const timeCell = document.getElementById(timeZoneId);
+//     if (timeCell) {
+//       timeCell.textContent = timeString;
+//     } else {
+//       console.error(`Element with id ${timeZoneId} not found`);
+//     }
+//   });
+// }
 
 // Call the function to update the date and time immediately
 updateDateTime();
@@ -365,4 +312,4 @@ getCurrentTimeZone();
 // Optionally, update the date and time every second
 setInterval(updateDateTime, 1000);
 //setInterval(updateCountryTimes, 1000);
-setInterval(updateAllTimeZones, 1000);
+//setInterval(updateAllTimeZones, 1000);
