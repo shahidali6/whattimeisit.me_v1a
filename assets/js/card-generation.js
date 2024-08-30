01416dc01728609c4a6a9bfa611791457ddcc738
Shahid Saddique <shahidali6@gmail.com>
2024-08-30 12:07:46 +0500

    duplicate method removed createAllTimeZoneCards()

--- assets/js/card-generation.js
+++ assets/js/card-generation.js
@@ -6,47 +6,6 @@ function removeAllChildsOfElement() {
   }
 }
 
-function createAllTimeZoneCards() {
-  const tableBody = document.getElementById("card-row");
-  const cardTemplate = document.getElementById("first-card");
-  const removeCard = document.getElementById("first-card");
-  removeCard.remove();
-
-  timeZonesAll1.forEach((timeZone, index) => {
-    // Split time zone into region and city
-    const region = timeZone.substring(0, timeZone.indexOf("/"));
-    const city = timeZone
-      .substring(timeZone.indexOf("/") + 1, timeZone.length)
-      .replace(/\//g, "--")
-      .replace(/_/g, "-"); // Replace '/' with ' - ' for better display
-    //console.log(index + " Region:[" + region+ "] City:[" + city+"]");
-
-    // Clone the card element
-    const newCard = cardTemplate.cloneNode(true);
-    newCard.id = ""; // Clear the id attribute to avoid duplicates
-    newCard.querySelector("#time").id = "time" + city;
-
-    // Update the card content
-    const now = new Date();
-    const options = {
-      timeZone,
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    };
-    const timeString = new Intl.DateTimeFormat("en-US", options).format(now);
-
-    // Assuming the card has elements to display the time zone and current time
-    newCard.querySelector(".time-zone-city").textContent = city;
-    newCard.querySelector(".time-zone-region").textContent = region;
-    newCard.querySelector(".current-time").textContent = timeString;
-    newCard.querySelector(".current-time").textContent = timeString;
-
-    // Append the cloned card to the table body
-    tableBody.appendChild(newCard);
-  });
-}
-
 function createCard(timeZone) {
   // Split time zone into region and city
   const region = timeZone.substring(0, timeZone.indexOf("/"));
@@ -177,25 +136,6 @@ function createPacificTimeZoneCards() {
     cardRow.appendChild(newCard);
   });
 }
-
-// function updatePacificZoneTime() {
-//     timeZonesAll1.forEach(timeZone => {
-//         const now = new Date();
-//         const options = { timeZone, hour: '2-digit', minute: '2-digit', second: '2-digit' };
-//         const timeString = new Intl.DateTimeFormat('en-US', options).format(now);
-//         const timeZoneId = `time${timeZone.replace(/\//g, '_')}`; // Replace '/' with '_' for valid HTML id
-//         const timeCell = document.getElementById(timeZoneId);
-//         if (timeCell) {
-//             timeCell.textContent = timeString;
-//         } else {
-//             console.error(`Element with id ${timeZoneId} not found`);
-//         }
-//     });
-// }
-
-// Call the function to update the date and time immediately
-//updatePacificZoneTime();
-//createAllTimeZoneCards();
 
 //setInterval(createAllTimeZoneCards, 1000);
 createAsiaTimeZoneCards();
